62b5a45c3a60b171c11fffd27d399e7febf22d32
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-08-25 14:10:06 +0530

    Enabled react compiler

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,8 +10,9 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'eslint-plugin-react-compiler'],
   rules: {
+    'react-compiler/react-compiler': "error",
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const ReactCompilerConfig = {/* ... */ };
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [
+        [
+          "babel-plugin-react-compiler",
+           ReactCompilerConfig
+          ]
+        ],
+    },
+  })],
 })
